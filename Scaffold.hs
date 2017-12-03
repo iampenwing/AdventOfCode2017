e73236e008ee28d143dcc2c29d0a85d33aa66c79
penwing (Alex Lambert) <aoc@penwing.me.uk>
2017-12-03 10:02:09 +0000

    Code and details for Day 3 Puzzle 1

--- Scaffold.hs
+++ Scaffold.hs
@@ -8,13 +8,20 @@
 
 module Scaffold where
 
-       charIntOffset :: Int
-       charIntOffset = fromEnum '0'
+charIntOffset :: Int
+charIntOffset = fromEnum '0'
 
-       stringToNumbers :: [Char] -> [Int]
-       stringToNumbers l = map (\x -> x - charIntOffset) (map fromEnum l)
+stringToNumbers :: [Char] -> [Int]
+stringToNumbers l = map (\x -> x - charIntOffset) (map fromEnum l)
 
-       rotateList :: Int -> [Int] -> [Int]
-       rotateList n l = (drop n l) ++ (take n l) 
+rotateList :: Int -> [Int] -> [Int]
+rotateList n l = (drop n l) ++ (take n l) 
 --       rotateList 0 l = l
 --       rotateList n (x:xs) = rotateList (n - 1) (xs ++ [x])
+
+extractValue :: Int -> [(Int, Int)] -> Int
+extractValue k [] = 0
+extractValue k (pair:pairs) 
+	     | k == (fst pair)  = snd pair
+	     | otherwise        = extractValue k pairs
+
